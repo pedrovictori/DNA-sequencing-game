a3afb10b72690bfc6449ffaffb857cacbf1a9f04
Pedro Victori <fosforilasa@gmail.com>
2017-04-16 14:16:54 +0100

    working fragments drawing

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application{
         AnchorPane root = new AnchorPane();
         
         Sequence seq = Sequence.generator(100);
+        List<Sequence> frags = seq.generateFixedSizedFragments(30, 5);
         
         //draw mould sequence
         List<Rectangle> seqDrawing = new ArrayList<Rectangle>();
@@ -37,9 +38,25 @@ public class Main extends Application{
         
         root.getChildren().addAll(seqDrawing);
         
+        //draw fragments
+        List<Rectangle> fragDrawing = new ArrayList<Rectangle>();
         
-        List<Sequence> frags = seq.generateFixedSizedFragments(30, 5);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        for (int i = 0; i < frags.size(); i++) {
+			double yPos = sqSize*(2*i+3);
+			Sequence sequence = frags.get(i);
+			
+			for (int j = 0; j < sequence.size(); j++) {
+				double xPos = sqSize*(j+1);
+				Rectangle rectangle = new Rectangle(xPos, yPos, sqSize, sqSize); //xpos, ypos, width, height
+	        	rectangle.setFill(seq.get(j).getColor());
+	        	fragDrawing.add(rectangle);
+			}
+		}
+        
+        root.getChildren().addAll(fragDrawing);
+        
+        primaryStage.setScene(new Scene(root));
+        primaryStage.sizeToScene();
         primaryStage.show();
     }
 }
